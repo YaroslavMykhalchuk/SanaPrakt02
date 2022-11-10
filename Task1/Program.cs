d5e19b8286203ed4e95ede7df01a0d0a227e76e5
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2022-11-10 03:07:12 +0200

    Change three random message

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Random message 1");
+Console.WriteLine("Random message 2");
+Console.WriteLine("Random message 3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
